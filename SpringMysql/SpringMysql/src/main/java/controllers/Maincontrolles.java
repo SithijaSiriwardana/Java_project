523d03b5350f6984fe79701251f8f0a4b5435ce3
Sithija Siriwardana <sithijalawan@gmail.com>
2019-09-14 19:57:01 +0530

    Welcome page added

--- SpringMysql/SpringMysql/src/main/java/controllers/Maincontrolles.java
+++ SpringMysql/SpringMysql/src/main/java/controllers/Maincontrolles.java
@@ -1,5 +1,17 @@
 package controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
 public class Maincontrolles {
+
+
+	    @RequestMapping("/")
+	    public ModelAndView doHome() {
+	        ModelAndView mv = new ModelAndView("index");
+	        return mv;
+	    }
 
 }
